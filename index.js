402c00cacea073181230e29bec652945fb57589d
Karen Olson <karmarie@gmail.com>
2021-08-06 12:51:29 -0500

    commit tree 7ad54e83fa053bc9dd0959d01d94ad96911dbd2f

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Add your code here
+function submitData(name, email) {
+  const userObj = {
+    name: `${name}`,
+    email: `${email}`,
+  };
+
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(userObj),
+  };
+
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((response) => response.json())
+    .then((data) => appendID(data))
+    .catch((errorObj) => handleError(errorObj));
+}
+
+function appendID(data) {
+  const id = data["id"];
+  const body = document.querySelector("body");
+  body.innerHTML = `<p>${id}</p>`;
+}
+
+function handleError(errorObj) {
+  const msg = errorObj["message"];
+  const body = document.querySelector("body");
+  body.innerHTML = `<p>${msg}</p>`;
+}
+
+//submitData(a, "a@aol.com");
